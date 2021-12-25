f67d8de166b3d666c57065344454fbaa87eef9bb
thecoderpunk <ravisingh02082001@gmail.com>
2021-12-26 00:34:39 +0530

    first commit

---
+++ server/index.js
@@ -0,0 +1,26 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+
+import postRoutes from "./routes/posts.js";
+
+const app = express();
+dotenv.config();
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+app.use('/posts', postRoutes);
+
+app.get('/', (req, res) => {
+    res.send('Hello to souvenir API');
+});
+
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(process.env.CONNECTION_URL)
+    .then(()=> app.listen(PORT, ()=> console.log(`server running on port: ${PORT} `)))
+    .catch((error)=> console.log(error.message));
